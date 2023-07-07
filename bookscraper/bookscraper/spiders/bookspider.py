27f69742f6de79ff41cd8e1b7a676f5e0a0fe07f
Somya <isomya13@gmail.com>
2023-07-08 00:45:24 +0530

    stored data in csv and json files

--- bookscraper/bookscraper/spiders/bookspider.py
+++ bookscraper/bookscraper/spiders/bookspider.py
@@ -10,9 +10,25 @@ class BookspiderSpider(scrapy.Spider):
         books = response.css('article.product_pod')
 
         for book in books:
-            yield{
-                'name' : book.css('h3 a::text').get(),
-                'price': book.css('.product_price .price_color::text').get(),
-                'url': book.css('h3 a').attrib['href'],
-            }
-        next_page = response.css('li.next a ::attr(href)').get()
+
+            relative_url = book.css('h3 a ::attr(href)').get()
+
+            if 'catalogue/' in relative_url:
+             book_url= 'https://books.toscrape.com/'+relative_url
+
+            else :
+             book_url='https://books.toscrape.com/catalogue'+ relative_url
+
+            yield response.follow(book_url, callback=self.parse)
+
+    
+    def parse_book_page(self, response):
+       
+       table_rows = response.css("table tr")
+
+       yield{
+          'url': response.url,
+          'title': response.css('.product_main h1::text').get(),
+          'product_type': table_rows[1].css("td ::text").get()
+       }
+        
